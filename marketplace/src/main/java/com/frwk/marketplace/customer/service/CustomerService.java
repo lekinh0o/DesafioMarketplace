2d4d466e663c750979bbb3b3d769ec9e1b1282e8
Alex Vago Morais <Alexvago15@gmail.com>
2023-02-07 13:58:43 -0300

    cria mais 3 metodos  updateCustomer/findByCpf/listCustomers

--- marketplace/src/main/java/com/frwk/marketplace/customer/service/CustomerService.java
+++ marketplace/src/main/java/com/frwk/marketplace/customer/service/CustomerService.java
@@ -1,5 +1,8 @@
 package com.frwk.marketplace.customer.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -31,4 +34,26 @@ public class CustomerService {
         }
     }
 
+    public CustomerDTO save(CustomerDTO customerDTO) throws InvalidClientException {
+        Customer customer = this.repository.findByIdentificationCode(customerDTO.getCpf());
+        if (customer != null) {
+            customer.setName(customerDTO.getNome());
+            customer.setBirthDate(customerDTO.getDataNascimento());
+            customer.setEmail(customerDTO.getEmail());
+            this.repository.save(customer);
+            return customerDTO;
+        }
+        throw new InvalidClientException("Cliente não esta cadastrado na sistema");
+    }
+
+    public CustomerDTO findByIdentificationCode(String code) {
+        Customer customer = this.repository.findByIdentificationCode(code);
+        return this.mapper.mapEntityFromDTO(customer);
+    }
+
+    public List<CustomerDTO> listCustomers() {
+        return this.repository.findAll()
+                .stream().map(customer -> this.mapper.mapEntityFromDTO(customer))
+                .collect(Collectors.toList());
+    }
 }
